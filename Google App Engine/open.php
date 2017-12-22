e807fdb13430e537b58aeb1166a8bfdf9325853b
JH-YUN <34766520+JH-YUN@users.noreply.github.com>
2017-12-22 21:57:33 +0900

    Update open.php

--- Google App Engine/open.php
+++ Google App Engine/open.php
@@ -10,15 +10,10 @@ $sql="SELECT * FROM dl_user WHERE nfc_uid='{$nfc_id}'"; //nfc_id가 DB에 저장
 $result=mysqli_query($conn,$sql);
 $row=mysqli_fetch_assoc($result);
 if($result->num_rows!=0){ //저장된 값이라면
-  if($row['grade']=='null' && $row['user']=='null'){//저장된 값이지만 user혹은 grade가 null일땐 문이 열리지 않음->미인증
-    echo "noregister";
-  }
-  else{
   echo "ooopen"; //문열림
   $sql="INSERT INTO dl_record(nfc_uid,date) VALUES('{$nfc_id}',now())";
   mysqli_query($conn,$sql);
   }
-}
 else{   //저장되지 않은 값이라면
   echo "cloose"; //안열림
 }
